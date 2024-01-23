e8e81f4a7e8314b07f595827a0f3f8dd2e918ef6
Himanshu@Dalal <himanshuapachertr160@gmail.com>
2024-01-23 06:10:13 +0530

    Committing White Spaces Program

---
+++ src/Logical_Programs/C_White_Spaces_03.java
@@ -0,0 +1,26 @@
+package Logical_Programs;
+
+public class C_White_Spaces_03 
+{
+	public static void main(String[]args)
+	{
+		String A = "A B C D E F G H I J K L M N O P Q R T S U V W X Y Z";
+		int Count = 0;
+		
+		for(int i=0; i<=A.length()-1; i++)
+		{
+			char C = A.charAt(i);
+			if(C==' ')
+			{
+				Count++;
+			}
+			
+		}
+		
+		System.out.println(Count);
+		
+		
+	}
+
+
+}
